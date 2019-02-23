458cd7bcef21eafe2e6e547f8af11653472a6e15
justin <jjzwink@gmail.com>
2019-02-23 11:09:36 -0500

    added 2 more requests
    added eleanor and dmax1

---
+++ IsuzuDmax hi lander 2010/__resource.lua
@@ -0,0 +1,18 @@
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+
+files {
+'data/vehicles.meta'
+'data/carcols.meta'
+'data/carvariations.meta'
+'data/handling.meta'
+'data/vehiclelayouts.meta'
+'data/sounds/suv_1_eur_v6.awc'
+       }
+data_file 'VEHICLE_METADATA_FILE' 'data/vehicles.meta'
+data_file 'CARCOLS_FILE' 'data/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'data/carvariations.meta'
+data_file 'HANDLING_FILE' 'data/handling.meta'
+data_file 'VEHICLE_LAYOUTS_FILE' 'data/vehiclelayouts.meta'
+data_file 'AUDIO_WAVEPACK' 'data/sounds'
+
+  is_els 'false'
---
+++ ShelbyGT500Eleanor/__resource.lua
@@ -0,0 +1,10 @@
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+
+files {
+'data/vehicles.meta'
+'data/handling.meta'
+       }
+data_file 'VEHICLE_METADATA_FILE' 'data/vehicles.meta'
+data_file 'HANDLING_FILE' 'data.handling.meta'
+
+  is_els 'false'
