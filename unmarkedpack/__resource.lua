bc2164ba1373c3d6fdd130c18a5b51bd5932668d
justin <jjzwink@gmail.com>
2019-02-22 22:51:22 -0500

    added vehicles
    added the honda50 the Ninja and a small pack if slicktop unmarked ELS vics

---
+++ unmarkedpack/__resource.lua
@@ -0,0 +1,17 @@
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+
+files {
+'data/vehicles.meta',
+'data/ELSVCF/umvic2s.xml',
+'data/ELSVCF/umtah13s.xml',
+'data/ELSVCF/umchar14s.xml',
+'data/ELSVCF/umtah15s.xml',
+'data/ELSVCF/umfpis16s.xml',
+'data/ELSVCF/umchar18s.xml',
+'data/ELSVCF/umfpiu16s.xml'
+
+       }
+data_file 'VEHICLE_METADATA_FILE' 'data/vehicles.meta'
+
+
+  is_els 'true'
---
+++ unmarkedpack/readme.txt
@@ -0,0 +1,12 @@
+These vehicles are imported to FiveM By WolfeLabs Productions
+
+Call names:
+
+umchar14s
+umchar18s
+umdur18s
+umfpis16s
+umfpiu16s
+umtah13s
+umtah15s
+umvic2s
